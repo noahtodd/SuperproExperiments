c7dffdad8285fe0948350fe3d581968c891e3df4
Noah Todd <noahtodd7@gmail.com>
2014-08-14 07:47:06 -0400

    Delete unnecessary drivers

--- comm-testing.c
+++ comm-testing.c
@@ -4,41 +4,51 @@
 
 task comm(){
 	int inputdata;
-  ubyte outputdata;
   bool NXTprocessing = 0; // pin 3
   ubyte arduinoUpdate = 0b00000100;// pin 2
   int frameSize = 10;
 	nxtDisplayTextLine(1, "initialized task");
+
   // set pins 3,4,5,6,7 as outputs
   HTSPBsetupIO(HTSPB, 0b11111000);
 	//change NXT processing pin to low
   HTSPBwriteIO(HTSPB, 0b11110000);
+
   //wait for Arduino pin to pull high. arduino is pin 2
   nxtDisplayTextLine(2, "waiting for arduino");
-  while(!HTSPBreadIO(HTSPB, 0b00000100)){wait1Msec(1);}
+  while(HTSPBreadIO(HTSPB, 0b00000100) != 4){
+  	nxtDisplayTextLine(5, "arduino pin: %d", HTSPBreadIO(HTSPB, 0b00000100));
+  	wait1Msec(1);
+  }
+
   while(true) {
   	inputdata = 0;
     //for loop that runs once for each packet in a frame
   	for (int i = 0; i < 10; i++){
-  		nxtDisplayTextLine(3, "cycle %d", i);
-  		//read input pin / pins
-  		inputdata += HTSPBreadIO(HTSPB, 0x01);
-  		//use left-hand operator to process data
-  		inputdata = inputdata << 1;
-  		//change NXT processing pin
-  		NXTprocessing = !NXTprocessing;
-  		HTSPBwriteIO(HTSPB, NXTprocessing << 3);
   		//wait for the arduino to change the arduino update pin
   		nxtDisplayTextLine(3, "cycle %d, waiting for arduino", i);
   		while(arduinoUpdate == HTSPBreadIO(HTSPB, 0b00000100)){
   			wait1Msec(1);
   		}
+  		nxtDisplayTextLine(3, "cycle %d", i);
+  		//read input pin / pins
+  		inputdata += HTSPBreadIO(HTSPB, 0x01);
+  		nxtDisplayTextLine(4, "input 1: %d", HTSPBreadIO(HTSPB, 0x01));
+  		//use left-hand operator to process data
+  		inputdata = inputdata << 1;
+  		nxtDisplayTextLine(5, "input 1: %d", inputdata);
+  		//change NXT processing pin
+  		NXTprocessing = !NXTprocessing;
+  		HTSPBwriteIO(HTSPB, NXTprocessing << 3);
   		//change arduinoUpdate
-
+  		if (arduinoUpdate == 4)
+  			arduinoUpdate = 0;
+  		else
+  			arduinoUpdate = 4;
   	}
   	//display data
-
-  	//apportion data to values usable in the rest of the program
+  	nxtDisplayTextLine(7, "Data: %d", inputdata);
+  	//TODO apportion data to values usable in the rest of the program
   }
 }
 
